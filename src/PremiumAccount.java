326152d1d8d592c0bb8de048cf1d37c8a9a23ddc
Laura Safdie <flsafdie@gmail.com>
2023-01-25 05:02:17 -0500

    implemented premium account

--- src/PremiumAccount.java
+++ src/PremiumAccount.java
@@ -3,26 +3,33 @@
  * that they do not have credit limit - we can withdraw as much as we want.
  */
 public class PremiumAccount implements IAccount {
+    int accountNumber;
+    double accountBalance;
+
     /**
      * This constructor gets as an argument the account number
      * represented as an integer.
      * @param accountNumber the account number
      */
     public PremiumAccount(int accountNumber) {
+        this.accountNumber = accountNumber;
+        accountBalance = 0;
     }
 
     public void Deposit(double amount) {
+        accountBalance += amount;
     }
 
     public double Withdraw(double amount) {
-        return 0;
+        accountBalance -= amount;
+        return amount;
     }
 
     public double GetCurrentBalance() {
-        return 0;
+        return accountBalance;
     }
 
     public int GetAccountNumber() {
-        return 0;
+        return accountNumber;
     }
 }
