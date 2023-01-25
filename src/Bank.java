5b4b32e9d253acb5fdeeacc468ad2f82b50e6bd0
Laura Safdie <flsafdie@gmail.com>
2023-01-25 05:10:38 -0500

    implemented bank

--- src/Bank.java
+++ src/Bank.java
@@ -1,23 +1,53 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bank implements IBank {
-    public Bank() { }
+    List<IAccount> accountList;
+
+    public Bank() {
+        accountList = new ArrayList<>();
+    }
 
     public void OpenAccount(IAccount account) {
+        accountList.add(account);
     }
 
     public void CloseAccount(int accountNumber) {
+        for(int i = 0; i < accountList.size(); i++) {
+            IAccount account = accountList.get(i);
+            if (account.GetAccountNumber() == accountNumber) {
+                if (account.GetCurrentBalance() < 0) {
+                    System.out.println("The account cannot be closed due to debt.");
+                }
+                else {
+                    accountList.remove(i);
+                    break;
+                }
+            }
+        }
     }
 
     public List<IAccount> GetAllAccounts() {
-        return null;
+        return accountList;
     }
 
     public List<IAccount> GetAllAccountsInDebt() {
-        return null;
+        List<IAccount> accountsInDebt = new ArrayList<>();
+        for(IAccount account : accountList) {
+            if(account.GetCurrentBalance() < 0) {
+                accountsInDebt.add(account);
+            }
+        }
+        return accountsInDebt;
     }
 
     public List<IAccount> GetAllAccountsWithBalance(double balanceAbove) {
-        return null;
+        List<IAccount> accountsWithBalance = new ArrayList<>();
+        for(IAccount account : accountList) {
+            if(account.GetCurrentBalance() > balanceAbove) {
+                accountsWithBalance.add(account);
+            }
+        }
+        return accountsWithBalance;
     }
 }
